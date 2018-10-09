5bcb6aadb616b1a0c9c44d4e0e1cf6b29df95de8
Reaganraice <40822871+Reaganraice@users.noreply.github.com>
2018-10-09 14:03:08 +0200

    Add files via upload

--- function.php
+++ function.php
@@ -1,7 +1,8 @@
 <?php
 
 // Add task
-function add($task) {
+function add($task,$date) {
+  $x=array($task,$date);
   // Initialize
   $tasklist = array();
   // Check if the session variable exist
@@ -10,19 +11,20 @@ function add($task) {
     $tasklist = $_SESSION['tasks'];
   }
   // Add our task to our array
-  array_push($tasklist, $task);
+  array_push($tasklist, $x);
   // Save our array back to our session
   $_SESSION['tasks'] = $tasklist;
 }
 
 // Delete task
-function delete($task) {
+function delete($task,$date) {
+  $x=array($task,$date);
   // Initialize our list
   $tasklist = $_SESSION['tasks'];
   // Check if the list is set
   if (isset($tasklist)) {
     // Find task in tasklist return position
-    $position = array_search($task, $tasklist);
+    $position = array_search($x, $tasklist);
     // If the position is found = > 0
     if ($position >= 0) {
       // delete task
@@ -47,3 +49,4 @@ function edit($task,$newtask){
 }
 
 ?>
+
--- index.php
+++ index.php
@@ -3,72 +3,75 @@ session_start();
 
 require 'function.php';
 
+ /*echo '$_GET: ';
+ print_r($_GET);
+ echo '$_POST: ';
+print_r($_POST);*/
+
+//print_r($_SESSION['tasks']);
 // [0] => array('name' => 'reagan', 'duedate' => '1 Jan 1994')
 
 // check if the delete and task is set in the GET / URL
 if (isset($_GET['delete']) && isset($_GET['task'])) {
   // Call delete function with task
-  delete($_GET['task']);
+  delete($_GET['task'],$_GET['duedate']);
+  // session_unset();
 }
 
 // check if the form has been submitted
 if (isset($_POST['submit'])) {
   // Call add function with task
-  add($_POST['task']);
+  add($_POST['task'],$_POST['duedate']);
 }
 
 
-//echo $_GET['task'];
+// echo $_GET['task'];
 
 $taskname = '';
 if (isset($_GET['task'])) {
   $taskname = $_GET['task'];
 } 
- 
 ?>
 ​
 <!DOCTYPE html>
 <html>
 <head>
  <title></title>
- <link rel="stylesheet" href="css/styles.css">
- 
+<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 <div class="todoform">
 <form action="index.php" method="POST" class='form' id="form">
-  <div class="inp">
+    <h1>TODO APP</h1>
+<div class="inp">
     <div>
-
-      <label>Add Task:</label> <input type="text" name="task" value="<?php echo  $taskname; ?>">
+    <label>Add Task:</label> <input type="text" name="task" value="<?php echo  $taskname; ?>">
     </div>
     <div>
-
-      <label>Due Date: </label><input type="date" name="duedate" requied >
+    <label>Due Date:</label><input type="date" name="duedate" required>
     </div>
     <div>
-
-      <input type="submit" name="submit" id="submit"/>
+    <input type="submit" name="submit" id="submit"/>
     </div>
-  </div>
-  <div>
-  
-    <ul>
-     <?php
-     if (isset($_SESSION['tasks'])) {
-      echo 'No of tasks: ' . count($_SESSION['tasks']);
-      foreach ($_SESSION['tasks'] as $task) {
-     ?>
-       <li><?php echo $task; ?> <a href="index.php?delete=1&task=<?php echo $task; ?>">delete</a> <a href="index.php?edit=0&task=<?php echo $task; ?>">edit</a><a href="<?php echo $date;?>"></a></li>
-     <?php
-      }
-     } ?>
-    </ul>
-  </div>
+ </div>
 </form>
-</div>
+
+<ul>
+ <?php
+ if (isset($_SESSION['tasks'])) {
+  echo 'No of tasks: ' . count($_SESSION['tasks']);
+  foreach ($_SESSION['tasks'] as $task) {
+ ?>
+   <li><?php echo $task[0] . "  /  ".$task[1]; ?> <a href="index.php?delete=1&task=<?php echo $task[0]; ?>&duedate=<?php echo $task[1] ?>">delete</a> <a href="index.php?edit=1&task=<?php echo $task[0]; ?>">edit</a></li>
+ <?php
+  }
+ } ?>
+</ul>
 </body>
 </html>
+
+
+
 
 
 
